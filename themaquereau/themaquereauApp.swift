fc3aee19984aef4d9acf44718713825fc0060191
david haioum <dav6983@gmail.com>
2021-01-26 22:53:01 +0100

    Initial Commit

---
+++ themaquereau/themaquereauApp.swift
@@ -0,0 +1,17 @@
+//
+//  themaquereauApp.swift
+//  themaquereau
+//
+//  Created by david haioum on 26/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct themaquereauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
